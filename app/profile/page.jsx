71e2d42431a8c0ab213b0c609116b2a25737fd68
Travis J <tfauvelle@gmail.com>
2023-12-15 09:40:53 -0500

    update prompt post

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -8,6 +8,7 @@ import Profile from "@components/profile";
 const MyProfile = () => {
   const { data: session } = useSession();
   const [posts, setPosts] = useState([]);
+  const router = useRouter();
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -19,9 +20,11 @@ const MyProfile = () => {
     if (session?.user.id) fetchPosts();
   }, []);
 
-  const handleEdit = () => {};
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
 
-  const handleDelete = async () => {};
+  const handleDelete = async (post) => {};
 
   return (
     <Profile
